f219f9a17ec9de1da03d614a8136ba5bddba82a7
supernoir <kawohl.bentodesign@gmail.com>
2016-01-29 16:46:08 +0100

    tries to tie  boardprinter to cells variable

--- app.rb
+++ app.rb
@@ -1,39 +1,48 @@
 # Game of Life
 # Dayana / Conny
 
+require 'byebug'
 
 class Board
 
     @cells = []
-    @cell = 0
-
-10.times do
-    column = []
+    10.times do
+        column = []
     10.times do
         column.push(rand(2))
     end
     @cells.push(column)
 end
-# Outputting Cells Array
     puts @cells.inspect
-
 end
+
+
 
 class Game
 
-    def printBoard
+  attr_accessor :cells
+  
+  def initialize(board)
+  @board = board
+  @board.cells = cells
+  end
+  
+    def self.printBoard # Ask Stefan why self.* here
         10.times do
-            10.times { print "A" + " " }
+            debugger
+            10.times { print cells }
         print "\n"
+        end
     end
-
-
+    
+   
 end
     
-end
 
 Board.new
+
 Game.new
+Game.printBoard
 
 
 
