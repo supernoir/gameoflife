96b357696f1ab2d91695215f10c1457528c216c0
supernoir <kawohl.bentodesign@gmail.com>
2016-02-02 17:37:06 +0100

    creates the nested while loops and uses a global variable cells

--- app.rb
+++ app.rb
@@ -4,16 +4,15 @@
 require 'byebug'
 
 class Board
-
-    @cells = []
+    $cells = []
     10.times do
         column = []
     10.times do
         column.push(rand(2))
     end
-    @cells.push(column)
+    $cells.push(column)
 end
-    puts @cells.inspect
+    #puts @cells.inspect
 end
 
 
@@ -22,25 +21,33 @@ class Game
 
   attr_accessor :cells
   
-  def initialize(board)
-  @board = board
-  @board.cells = cells
+  def initialize
+  #board = Board.new
+  #@cells = cells
   end
   
-    def self.printBoard # Ask Stefan why self.* here
-        10.times do
-            debugger
-            10.times { print cells }
-        print "\n"
+  
+  def self.printBoard
+  
+  a = 0
+    while a < 10 do   
+    b = 0
+        while b < 10 do
+            #debugger
+            print $cells[a][b]
+            print " "
+            b += 1
+        end
+        print "\n"   
+        a += 1
         end
     end
-    
+
    
 end
     
 
 Board.new
-
 Game.new
 Game.printBoard
 
