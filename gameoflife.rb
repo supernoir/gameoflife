aa637c267e2d2571855ca826a03457e6d7234848
supernoir <kawohl.bentodesign@gmail.com>
2016-02-12 15:59:47 +0100

    counting live neighbors woooooorks

--- gameoflife.rb
+++ gameoflife.rb
@@ -17,16 +17,44 @@ class Grid
          puts true
       else
          puts false
+      end
    end
+
+
+   def live_neighbors(a,b)
+   count = 0
+    
+    if @cells[a-1][b] == true
+        count += 1
+    end
+    
+    if @cells[a+1][b] == true
+        count += 1
+    end
+    
+    if @cells[a][b-1] == true
+        count += 1
+    end
+    
+    if @cells[a][b+1] == true
+        count += 1
+    end
+        
+    puts count
+    
+   end
+
+
+   def print_grid
+      puts @cells.inspect
+   end
+
 end
 
 
-def print_grid
-puts @cells.inspect
-end
 
 
-grid = Grid.new(4,3)
+grid = Grid.new(6,6)
 
 grid.set_cell(1,2)
 grid.set_cell(1,0)
@@ -35,9 +63,12 @@ grid.set_cell(0,0)
 grid.set_cell(3,1)
 grid.set_cell(2,2)
 grid.print_grid
-grid.live?(0,0)
+grid.live?(3,2)
 grid.live?(1,2)
-grid.live?(1,0)
-grid.live?(1,1)
+grid.live?(3,2)
+grid.live?(2,2)
 grid.live?(3,1)
 grid.live?(2,2)
+
+
+grid.live_neighbors(2,2)
